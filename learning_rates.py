fbf7c6a3f6f6bd5644ff4812aedea16c260eb271
Dragan <dragan@u.yale-nus.edu.sg>
2022-12-12 00:02:44 +0100

    Fixed piecewise_constant_decay() and generalized the terminology to use for more than learning rates (e.g. differences)

--- learning_rates.py
+++ learning_rates.py
@@ -1,154 +1,154 @@
 """
-The ``learning_rates`` module houses ``LearningRates`` class that produces learning rates
-for gradient descent and other optimizers that utilize them.
+The ``schedules`` module houses ``Schedules`` class that produces sequences used in
+gradient descent and other optimizers for variable learning rates and other hyperparameters
 
 :raises ValueError: Error if there is more or less than exactly one more element of `values` that `boundaries`
-:return: Array of learning rates with each element being a learning rate for each epoch
+:return: Sequence of values with each element being a value (e.g. learning rate or difference) for each epoch
 :rtype: ndarray
 """
 
 import numpy as np
 
 
-class LearningRates():
+class Schedules():
     """
-    LearningRates class.
+    Schedules class.
 
     Instance variables:
 
-    - ``decay_steps`` - int
+    - ``n_steps`` - int
     - ``steps`` - ndarray
     """
 
     def __init__(self,
-                 decay_steps) -> None:
+                 n_steps) -> None:
         """
-        Initializes Learning Rates Generator Object.
+        Initializes Schedules Object.
 
-        :param decay_steps: Number of decay steps. Must be equal to the number of epochs of the algorithm
-        :type decay_steps: int
+        :param n_steps: Number of decay steps. Must be equal to the number of epochs of the algorithm
+        :type n_steps: int
         """
 
-        self.decay_steps = decay_steps
-        self.steps = np.linspace(0, decay_steps, decay_steps)
+        self.n_steps = n_steps
+        self.steps = np.linspace(0, n_steps, n_steps)
 
     def exponential_decay(self,
-                          initial_learning_rate,
+                          initial_value,
                           decay_rate,
                           staircase = False):
         """
-        Learning rate with exponential decay.
+        Sequence with exponential decay.
 
-        :param initial_learning_rate: Initial learning rate
-        :type initial_learning_rate: float
+        :param initial_value: Initial value of the sequence
+        :type initial_value: float
         :param decay_rate: Rate of decay
         :type decay_rate: float
-        :param staircase: If True decay the learning rate at discrete intervals, defaults to False
+        :param staircase: If True decay the sequence at discrete intervals, defaults to False
         :type staircase: bool, optional
-        :return: Array of learning rates with each element being a learning rate for each epoch
+        :return: Sequence of values with each element being a value (e.g. learning rate or difference) for each epoch
         :rtype: ndarray
         """
 
         if staircase is True:
-            learning_rates = initial_learning_rate*np.power(decay_rate, np.floor(np.divide(self.steps, self.decay_steps)))
+            sequence = initial_value*np.power(decay_rate, np.floor(np.divide(self.steps, self.n_steps)))
         else:
-            learning_rates = initial_learning_rate*np.power(decay_rate, np.divide(self.steps, self.decay_steps))
+            sequence = initial_value*np.power(decay_rate, np.divide(self.steps, self.n_steps))
 
-        return learning_rates
+        return sequence
 
     def cosine_decay(self,
-                     initial_learning_rate,
+                     initial_value,
                      alpha):
         """
-        Learning rate with cosine decay.
+        Sequence with cosine decay.
 
-        :param initial_learning_rate: Initial learning rate
-        :type initial_learning_rate: float
-        :param alpha: Minimum learning rate value as a fraction of initial_learning_rate
+        :param initial_value: Initial value of the sequence
+        :type initial_value: float
+        :param alpha: Minimum sequence value as a fraction of initial_value
         :type alpha: float
-        :return: Array of learning rates with each element being a learning rate for each epoch
+        :return: Sequence of values with each element being a value (e.g. learning rate or difference) for each epoch
         :rtype: ndarray
         """
 
-        steps = np.minimum(self.steps, self.decay_steps)
-        cosine_decay = 0.5 * (1 + np.cos(np.multiply(np.pi, np.divide(steps, self.decay_steps))))
+        steps = np.minimum(self.steps, self.n_steps)
+        cosine_decay = 0.5 * (1 + np.cos(np.multiply(np.pi, np.divide(steps, self.n_steps))))
         decayed = (1 - alpha) * cosine_decay + alpha
 
-        learning_rates = initial_learning_rate * decayed
+        sequence = initial_value * decayed
 
-        return learning_rates
+        return sequence
 
     def inverse_time_decay(self,
-                           initial_learning_rate,
+                           initial_value,
                            decay_rate,
                            staircase = False):
         """
-        Learning rate with inverse time decay
+        Sequence with inverse time decay
 
-        :param initial_learning_rate: Initial learning rate
-        :type initial_learning_rate: float
+        :param initial_value: Initial value of the sequence
+        :type initial_value: float
         :param decay_rate: Rate of decay
         :type decay_rate: float
-        :param staircase: If True decay the learning rate at discrete intervals, defaults to False
+        :param staircase: If True decay the sequence at discrete intervals, defaults to False
         :type staircase: bool, optional
-        :return: Array of learning rates with each element being a learning rate for each epoch
+        :return: Sequence of values with each element being a value (e.g. learning rate or difference) for each epoch
         :rtype: ndarray
         """
 
         if staircase is True:
-            learning_rates = np.divide(initial_learning_rate,
-                                      (1 + np.multiply(decay_rate, np.floor(np.divide(self.steps, self.decay_steps)))))
+            sequence = np.divide(initial_value,
+                                      (1 + np.multiply(decay_rate, np.floor(np.divide(self.steps, self.n_steps)))))
         else:
-            learning_rates = np.divide(initial_learning_rate,
-                                      (1 + np.multiply(decay_rate, np.divide(self.steps, self.decay_steps))))
+            sequence = np.divide(initial_value,
+                                      (1 + np.multiply(decay_rate, np.divide(self.steps, self.n_steps))))
 
-        return learning_rates
+        return sequence
 
     def polynomial_decay(self,
-                         initial_learning_rate,
-                         end_learning_rate,
+                         initial_value,
+                         end_value,
                          power,
                          cycle = False):
         """
-        Learning rate withpolynomial decay.
+        Sequence with polynomial decay.
 
-        :param initial_learning_rate: Initial learning rate
-        :type initial_learning_rate: float
-        :param end_learning_rate: The minimal end learning rate
-        :type end_learning_rate: float
+        :param initial_value: Initial value of the sequence
+        :type initial_value: float
+        :param end_value: The minimal end sequence value
+        :type end_value: float
         :param power: The power of the polynomial
         :type power: float
-        :param cycle: Whether or not it should cycle beyond self.decay_steps, defaults to False
+        :param cycle: Whether or not it should cycle beyond self.n_steps, defaults to False
         :type cycle: bool, optional
-        :return: Array of learning rates with each element being a learning rate for each epoch
+        :return: Sequence of values with each element being a value (e.g. learning rate or difference) for each epoch
         :rtype: ndarray
         """
 
         if cycle is True:
-            decay_steps = np.multiply(self.decay_steps, np.ceil(np.divide(self.steps, self.decay_steps)))
-            learning_rates = np.multiply((initial_learning_rate - end_learning_rate),
-                                         (1 - np.power(np.divide(self.steps, decay_steps), (power))
-                                         )) + end_learning_rate
+            n_steps = np.multiply(self.n_steps, np.ceil(np.divide(self.steps, self.n_steps)))
+            sequence = np.multiply((initial_value - end_value),
+                                         (1 - np.power(np.divide(self.steps, n_steps), (power))
+                                         )) + end_value
         else:
-            steps = np.minimum(self.steps, self.decay_steps)
-            learning_rates = np.multiply((initial_learning_rate - end_learning_rate),
-                                         (1 - np.power(np.divide(steps, self.decay_steps), (power))
-                                         )) + end_learning_rate
+            steps = np.minimum(self.steps, self.n_steps)
+            sequence = np.multiply((initial_value - end_value),
+                                         (1 - np.power(np.divide(steps, self.n_steps), (power))
+                                         )) + end_value
 
-        return learning_rates
+        return sequence
 
     def piecewise_constant_decay(self,
                                  boundaries,
                                  values):
         """
-        Learning rate with piecewise constant decay.
+        Sequence with piecewise constant decay.
 
         :param boundaries: Boundaries of the pieces
-        :type boundaries: float
-        :param values: values of learning rates in each of the pieces
-        :type values: float
+        :type boundaries: list
+        :param values: list of values in sequence in each of the pieces
+        :type values: list
         :raises ValueError: Error if there is more or less than exactly one more element of `values` that `boundaries`
-        :return: Array of learning rates with each element being a learning rate for each epoch
+        :return: Sequence of values with each element being a value (e.g. learning rate or difference) for each epoch
         :rtype: ndarray
         """
 
@@ -156,24 +156,27 @@ class LearningRates():
             raise ValueError("There should be only one value for each piece of array, \
                               i.e. there should be exactly one more element of `values` that `boundaries`")
 
-        learning_rates = np.zeros(len(self.steps))
-        for boundary in range(len(boundaries)):
-            learning_rates[boundaries[boundary]:boundaries[boundary+1]] = np.full(boundaries[boundary+1]-boundaries[boundary],
-                                                                                  values[boundary])
+        boundaries = np.append(0, boundaries)
+        boundaries = np.append(boundaries, self.n_steps)
 
-        return learning_rates
+        sequence = np.zeros(self.n_steps)
+        for value in range(len(values)):
+            sequence[boundaries[value]:boundaries[value+1]] = np.full(boundaries[value+1]-boundaries[value],
+                                                                            values[value])
+
+        return sequence
 
     def constant(self,
-                 learning_rate):
+                 value):
         """
-        Learning rate that stays constant
+        Constant sequence
 
-        :param learning_rate: Learning rate for each epoch
-        :type learning_rate: float
-        :return: Array of learning rates with each element being a learning rate for each epoch
+        :param value: Value for each epoch
+        :type value: float
+        :return: Sequence of values with each element being a value (e.g. learning rate or difference) for each epoch
         :rtype: ndarray
         """
 
-        learning_rates = np.full(len(self.steps), learning_rate)
+        sequence = np.full(len(self.steps), value)
 
-        return learning_rates
+        return sequence
