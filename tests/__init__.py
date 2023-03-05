9be68fd2242c989fa5fe081c469b9e6d14208eb2
Dragan <dragan@u.yale-nus.edu.sg>
2023-03-05 23:58:57 +0100

    Initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+Difference Gradient Descent
+
+Flexible Gradient Descent implementation for problems where gradient of an objective function cannot be found analytically.
+"""
+
+__version__ = "0.1.0"
+__author__ = "github.com/draktr"
