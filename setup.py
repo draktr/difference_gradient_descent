fa91d5e7563f8795665ebbbd170246bca4c3f4fa
Dragan <dragan@u.yale-nus.edu.sg>
2023-03-30 00:13:24 +0200

    Added new classifiers and updated other details after package name change

--- setup.py
+++ setup.py
@@ -1,26 +1,42 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    desc = f.read()
+
 setup(
-    name="difference_gradient_descent",
+    name="fdgd",
     version="0.1.0",
-    description="Flexible Gradient Descent implementation for problems where gradient of an objective function cannot be found analytically.",
-    url="https://github.com/draktr/difference_gradient_descent",
-    author="Dragan",
+    description="Finite Difference Gradient Descent (FDGD) can solve any function, including the ones without analytic form, by employing finite difference numerical differentiation within a gradient descent algorithm.",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/draktr/fdgd",
+    author="draktr",
     license="MIT License",
-    packages=["difference_gradient_descent"],
-    install_requires=["numpy",
-                      "pandas",
-                      "joblib"],
+    packages=["fdgd"],
+    python_requires=">=3.6",
+    install_requires=["numpy", "pandas", "joblib", "optschedule"],
+    keywords="optimization, gradient-descent, numerical-analysis, numerical-differentiation",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Education",
         "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Programming Language :: Python"
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
     ],
+    project_urls={
+        "Documentation": "https://fdgd.readthedocs.io/en/latest/",
+        "Issues": "https://github.com/draktr/fdgd/issues",
+    },
 )
