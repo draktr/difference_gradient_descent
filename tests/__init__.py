76e0e249cf15a1ceafd88ba82afe0da4e4c89fb6
Dragan <dragan@u.yale-nus.edu.sg>
2023-05-12 23:51:25 +0200

    Updated to reflect restructuring

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,12 @@ FinDi
 FinDi: Finite Difference Gradient Descent can optimize any function, including the ones without analytic form, by employing finite difference numerical differentiation within a gradient descent algorithm.
 """
 
-from findi.findi import descent, partial_descent, partially_partial_descent
+from findi.findi import (
+    descent,
+    partial_descent,
+    partially_partial_descent,
+    values_out,
+)
 
 __all__ = [s for s in dir() if not s.startswith("_")]
 
